5e0ec24695cd5b310d0544fce097bf1bcd909492
ruslan-mustafaev <therox5551998@gmail.com>
2025-09-16 13:40:35 +0200

    Updated index.ts

--- src/components/masterdashboard/index.ts
+++ src/components/masterdashboard/index.ts
@@ -1,11 +1,1 @@
-// components/masterdashboard/index.ts
-
-export { MasterDashboard } from './MasterDashboard';
-export { ProfilePhotos } from './ProfilePhotos';
-export { ProfileIdentity } from './ProfileIdentity';
-export { ProfileContact } from './ProfileContact';
-export { CalendarPreview } from './CalendarPreview';
-export { MasterCalendar } from './MasterCalendar';
-export { ProjectManagement } from './ProjectManagement';
-export { PaymentsSubscriptions } from './PaymentsSubscriptions';
-export * from './types';
+import { MasterDashboard } from './components/masterdashboard';
