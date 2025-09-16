e3b583394112796856bd7588ffd7f7bdd0a085f1
ruslan-mustafaev <therox5551998@gmail.com>
2025-09-16 12:49:59 +0200

    Updated index.ts

---
+++ src/components/masterdashboard/index.ts
@@ -0,0 +1,12 @@
+// components/masterdashboard/index.ts
+
+export { ProfilePhotos } from './ProfilePhotos';
+export { ProfileIdentity } from './ProfileIdentity';
+export { ProfileContact } from './ProfileContact';
+export { CalendarPreview } from './CalendarPreview';
+export { MasterCalendar } from './MasterCalendar';
+export { ProjectManagement } from './ProjectManagement';
+export { PaymentsSubscriptions } from './PaymentsSubscriptions';
+export * from './types';
+
+// MasterDashboard пока не экспортируем, так как его еще нужно создать правильно
