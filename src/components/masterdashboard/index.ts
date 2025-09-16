a4b0a6ecf13ff50baed3a87bb18fc4fd64e4d4b8
ruslan-mustafaev <therox5551998@gmail.com>
2025-09-16 13:40:42 +0200

    Updated index.ts

--- src/components/masterdashboard/index.ts
+++ src/components/masterdashboard/index.ts
@@ -1,1 +1,11 @@
-import { MasterDashboard } from './components/masterdashboard';
+export { default as CalendarPreview } from './CalendarPreview';
+export { default as MasterCalendar } from './MasterCalendar';
+export { default as MasterDashboard } from './MasterDashboard';
+export { default as ProfileContact } from './ProfileContact';
+export { default as ProfileIdentity } from './ProfileIdentity';
+export { default as ProfilePhotos } from './ProfilePhotos';
+export { default as ProjectManagement } from './ProjectManagement';
+export { default as PaymentsSubscriptions } from './PaymentsSubscriptions';
+
+// Если есть типы, можно экспортировать так:
+export * from './types';
