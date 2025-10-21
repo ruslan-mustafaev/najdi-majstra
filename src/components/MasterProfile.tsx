bef9ce322083661990b5fad9bbc27fbc2990dd81
ruslan-mustafaev <therox5551998@gmail.com>
2025-10-21 17:41:03 +0200

    Updated MasterProfile.tsx

--- src/components/MasterProfile.tsx
+++ src/components/MasterProfile.tsx
@@ -468,7 +468,7 @@ export const MasterProfile: React.FC<MasterProfileProps> = ({ master, onBack, is
                         className="flex items-center justify-center hover:opacity-80 transition-opacity"
                         title="Telegram"
                       >
-                        <img src="/svg_social/telegram_svg.svg" alt="Telegram" className="w-10 h-10" />
+                        <img src="/svg_social/telegram_svg.svg" alt="Telegram" className="w-11 h-11" />
                       </a>
                     )}
                     {(master as any).socialWhatsapp && (
